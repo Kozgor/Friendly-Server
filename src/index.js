4a88260cf92e2f6e541f4a4724318715faf2284a
Ihor <57453880+Kozgor@users.noreply.github.com>
2023-09-22 13:53:55 +0300

    Fix/expiration token updates (#10)
    * Feat: column route
    
    * Feat: column card model update
    
    * Fix: column schema, token expiration
    
    * Fix: column schema, route definition
    
    * Fix: column schema, route definition
    
    * Fix: column schema, route definition

--- src/index.js
+++ src/index.js
@@ -34,11 +34,7 @@ app.use('/auth', authRouter);
 
 app.use('/boards', boardRouter);
 
-app.use('/cards', cardRouter);
-
-app.use('/columns', columnRouter);
-
-app.use('/columns', columnRouter);
+app.use('/card', cardRouter);
 
 app.use('/columns', columnRouter);
 

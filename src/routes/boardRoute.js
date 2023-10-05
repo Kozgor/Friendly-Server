ebde401ac88765d8bc2c194d1cc5bd2a80a9870f
Ihor <57453880+Kozgor@users.noreply.github.com>
2023-10-05 10:36:31 +0300

    Feature/activate board (#18)
    * Fix: column schema, route definition
    
    * Feat: added board activation

--- src/routes/boardRoute.js
+++ src/routes/boardRoute.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.route('/new-board').post(BoardController.createBoard);
 router.route('/get-board').post(BoardController.getBoardById);
 router.route('/finalize-board').put(BoardController.setFinalizedBoard);
+router.route('/activate-board').put(BoardController.setActivatedBoard);
 router.route('/archive-board').put(BoardController.setArchivedBoard);
 router.route('/active').get(BoardController.getActiveBoard);
 router.route('/finalized').get(BoardController.getFinalizedBoard);

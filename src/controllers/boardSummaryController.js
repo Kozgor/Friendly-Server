e9868812f026667e45b0b2311ca3015bfc89b639
Ihor <57453880+Kozgor@users.noreply.github.com>
2023-12-08 19:39:35 +0200

    Hotfix/ssfr 81 create specific endpoint for board summary (#38)
    * Fix: column schema, route definition
    
    * hotfix: added board name field to board summary data response
    
    * hotfix: added board name field to board summary data response
    
    * hotfix: added board name field to board summary data response

--- src/controllers/boardSummaryController.js
+++ src/controllers/boardSummaryController.js
@@ -4,48 +4,57 @@ import ColumnCardModel from "../models/ColumnCard.js";
 import groupDataByColumnId from "../utils/groupDataByColumnId.js";
 
 export const getBoardSummary = async (req, res) => {
-    try {
-      const errors = validationResult(req);
-  
-      if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
-      }
-  
-      const cards = await ColumnCardModel.find({ boardId: req.body.boardId });
-      const board = await BoardModel.findById(req.body.boardId);
-      const sortedCards = groupDataByColumnId(cards);
+  try {
+    const errors = validationResult(req);
 
-      if (sortedCards) {
-        const allCards = Object.values(sortedCards).flat();
-        let trueCount = 0;
-        let falseCount = 0;
-
-        const boardSummaryData = allCards.map((card) => {
-          card.cardReactions.forEach((cardReaction) => {
-            cardReaction.isHappyReaction ? trueCount++ : falseCount++;
-          });
-          return {
-            boardName: board.name,
-            columnId: card.columnId,
-            cardComment: card.cardComment,
-            cardTags: card.cardTags ? card.cardTags.join(', ') : '',
-            cardReactions: {
-              happy: trueCount, unhappy: falseCount
-            },
-            cardAuthor: card.cardAuthor
-          }
-        });
-
-        res.json(boardSummaryData);
-
-        return;
-      }
-
-      res.json(boardSummaryData);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        message: "Summary getting error",
-      });
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
     }
+
+    const cards = await ColumnCardModel.find({ boardId: req.body.boardId });
+    const board = await BoardModel.findById(req.body.boardId);
+    const sortedCards = groupDataByColumnId(cards);
+
+    if (sortedCards || board.name) {
+      const allCards = Object.values(sortedCards).flat();
+      let trueCount = 0;
+      let falseCount = 0;
+
+      const getBoardSummaryData = () => {
+        if (allCards.length) {
+          return allCards.map((card) => {
+            card.cardReactions.forEach((cardReaction) => {
+              cardReaction.isHappyReaction ? trueCount++ : falseCount++;
+            });
+
+            return {
+              boardName: board.name,
+              columnId: card.columnId,
+              cardComment: card.cardComment,
+              cardTags: card.cardTags ? card.cardTags.join(', ') : '',
+              cardReactions: {
+                happy: trueCount, unhappy: falseCount
+              },
+              cardAuthor: card.cardAuthor
+            }
+          })
+        }
+
+        return [{ boardName: board.name }];
+      }
+
+  
+      const boardSummaryData = getBoardSummaryData();
+      res.json(boardSummaryData);
+
+      return;
+    }
+
+    res.json(boardSummaryData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Summary getting error",
+    });
+  }
 };
